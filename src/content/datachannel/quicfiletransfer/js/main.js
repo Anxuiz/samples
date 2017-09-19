a858ca25b43d5b346f4e5b65ffcce54cec75dc06
Steve Anton <steveanton@webrtc.org>
2017-09-19 13:06:07 +0200

    receive status too

--- src/content/datachannel/quicfiletransfer/js/main.js
+++ src/content/datachannel/quicfiletransfer/js/main.js
@@ -94,6 +94,7 @@ function handleConnected() {
   } else {
     fileInput.style.display = 'block';
     sendProgressPanel.style.display = 'block';
+    receiveProgressPanel.style.display = 'block';
   }
 }
 
@@ -169,6 +170,11 @@ function readFile(stream) {
   readChunks.push(chunk);
   readLength += chunk.length;
   scanForMetadata();
+  console.log(readLength);
+  if (readFileMetadata) {
+    console.log('hi');
+    receiveProgress.value = readLength;
+  }
   if (readFileMetadata && readLength >= readFileMetadata.metaLength + readFileMetadata.fileSize) {
     handleFileRead();
   } else {
@@ -192,8 +198,12 @@ function scanForMetadata() {
           fileName: reader.filename,
           metaLength: 6 + reader.filename.length,
         };
-        console.log('file metadata');
-        console.log(readFileMetadata);
+        readLength -= readFileMetadata.metaLength;
+        receiveProgress.max = reader.filesize;
+        readChunks[i] = new Uint8Array(chunk, j + 1);
+        for (; i >= 0; i--) {
+          readChunks.shift();
+        }
         return;
       }
     }
@@ -225,271 +235,10 @@ class MetadataReader {
 }
 
 function handleFileRead() {
+  var blob = new Blob(readChunks);
+  downloadAnchor.href = URL.createObjectURL(blob);
+  downloadAnchor.download = readFileMetadata.fileName;
+  downloadAnchor.textContent = 'Click to download \'' + readFileMetadata.fileName + '\' (' + readFileMetadata.fileSize + ' bytes)';
+  downloadAnchor.style.display = 'block';
   console.log('file read done!');
-}
-  
-
-function createConnection() {
-  var servers = null;
-  pcConstraint = null;
-
-  // Add localConnection to global scope to make it visible
-  // from the browser console.
-  window.localConnection = localConnection = new RTCPeerConnection(servers,
-      pcConstraint);
-  trace('Created local peer connection object localConnection');
-
-  sendChannel = localConnection.createDataChannel('sendDataChannel');
-  sendChannel.binaryType = 'arraybuffer';
-  trace('Created send data channel');
-
-  sendChannel.onopen = onSendChannelStateChange;
-  sendChannel.onclose = onSendChannelStateChange;
-  localConnection.onicecandidate = function(e) {
-    onIceCandidate(localConnection, e);
-  };
-
-  localConnection.createOffer().then(
-    gotDescription1,
-    onCreateSessionDescriptionError
-  );
-  // Add remoteConnection to global scope to make it visible
-  // from the browser console.
-  window.remoteConnection = remoteConnection = new RTCPeerConnection(servers,
-      pcConstraint);
-  trace('Created remote peer connection object remoteConnection');
-
-  remoteConnection.onicecandidate = function(e) {
-    onIceCandidate(remoteConnection, e);
-  };
-  remoteConnection.ondatachannel = receiveChannelCallback;
-
-  fileInput.disabled = true;
-}
-
-function onCreateSessionDescriptionError(error) {
-  trace('Failed to create session description: ' + error.toString());
-}
-
-function sendData() {
-  var file = fileInput.files[0];
-  trace('File is ' + [file.name, file.size, file.type,
-      file.lastModifiedDate
-  ].join(' '));
-
-  // Handle 0 size files.
-  statusMessage.textContent = '';
-  downloadAnchor.textContent = '';
-  if (file.size === 0) {
-    bitrateDiv.innerHTML = '';
-    statusMessage.textContent = 'File is empty, please select a non-empty file';
-    closeDataChannels();
-    return;
-  }
-  sendProgress.max = file.size;
-  receiveProgress.max = file.size;
-  var chunkSize = 16384;
-  var sliceFile = function(offset) {
-    var reader = new window.FileReader();
-    reader.onload = (function() {
-      return function(e) {
-        sendChannel.send(e.target.result);
-        if (file.size > offset + e.target.result.byteLength) {
-          window.setTimeout(sliceFile, 0, offset + chunkSize);
-        }
-        sendProgress.value = offset + e.target.result.byteLength;
-      };
-    })(file);
-    var slice = file.slice(offset, offset + chunkSize);
-    reader.readAsArrayBuffer(slice);
-  };
-  sliceFile(0);
-}
-
-function closeDataChannels() {
-  trace('Closing data channels');
-  sendChannel.close();
-  trace('Closed data channel with label: ' + sendChannel.label);
-  if (receiveChannel) {
-    receiveChannel.close();
-    trace('Closed data channel with label: ' + receiveChannel.label);
-  }
-  localConnection.close();
-  remoteConnection.close();
-  localConnection = null;
-  remoteConnection = null;
-  trace('Closed peer connections');
-
-  // re-enable the file select
-  fileInput.disabled = false;
-}
-
-function gotDescription1(desc) {
-  localConnection.setLocalDescription(desc);
-  trace('Offer from localConnection \n' + desc.sdp);
-  remoteConnection.setRemoteDescription(desc);
-  remoteConnection.createAnswer().then(
-    gotDescription2,
-    onCreateSessionDescriptionError
-  );
-}
-
-function gotDescription2(desc) {
-  remoteConnection.setLocalDescription(desc);
-  trace('Answer from remoteConnection \n' + desc.sdp);
-  localConnection.setRemoteDescription(desc);
-}
-
-function getOtherPc(pc) {
-  return (pc === localConnection) ? remoteConnection : localConnection;
-}
-
-function getName(pc) {
-  return (pc === localConnection) ? 'localPeerConnection' :
-      'remotePeerConnection';
-}
-
-function onIceCandidate(pc, event) {
-  getOtherPc(pc).addIceCandidate(event.candidate)
-  .then(
-    function() {
-      onAddIceCandidateSuccess(pc);
-    },
-    function(err) {
-      onAddIceCandidateError(pc, err);
-    }
-  );
-  trace(getName(pc) + ' ICE candidate: \n' + (event.candidate ?
-      event.candidate.candidate : '(null)'));
-}
-
-function onAddIceCandidateSuccess() {
-  trace('AddIceCandidate success.');
-}
-
-function onAddIceCandidateError(error) {
-  trace('Failed to add Ice Candidate: ' + error.toString());
-}
-
-function receiveChannelCallback(event) {
-  trace('Receive Channel Callback');
-  receiveChannel = event.channel;
-  receiveChannel.binaryType = 'arraybuffer';
-  receiveChannel.onmessage = onReceiveMessageCallback;
-  receiveChannel.onopen = onReceiveChannelStateChange;
-  receiveChannel.onclose = onReceiveChannelStateChange;
-
-  receivedSize = 0;
-  bitrateMax = 0;
-  downloadAnchor.textContent = '';
-  downloadAnchor.removeAttribute('download');
-  if (downloadAnchor.href) {
-    URL.revokeObjectURL(downloadAnchor.href);
-    downloadAnchor.removeAttribute('href');
-  }
-}
-
-function onReceiveMessageCallback(event) {
-  // trace('Received Message ' + event.data.byteLength);
-  receiveBuffer.push(event.data);
-  receivedSize += event.data.byteLength;
-
-  receiveProgress.value = receivedSize;
-
-  // we are assuming that our signaling protocol told
-  // about the expected file size (and name, hash, etc).
-  var file = fileInput.files[0];
-  if (receivedSize === file.size) {
-    var received = new window.Blob(receiveBuffer);
-    receiveBuffer = [];
-
-    downloadAnchor.href = URL.createObjectURL(received);
-    downloadAnchor.download = file.name;
-    downloadAnchor.textContent =
-      'Click to download \'' + file.name + '\' (' + file.size + ' bytes)';
-    downloadAnchor.style.display = 'block';
-
-    var bitrate = Math.round(receivedSize * 8 /
-        ((new Date()).getTime() - timestampStart));
-    bitrateDiv.innerHTML = '<strong>Average Bitrate:</strong> ' +
-        bitrate + ' kbits/sec (max: ' + bitrateMax + ' kbits/sec)';
-
-    if (statsInterval) {
-      window.clearInterval(statsInterval);
-      statsInterval = null;
-    }
-
-    closeDataChannels();
-  }
-}
-
-function onSendChannelStateChange() {
-  var readyState = sendChannel.readyState;
-  trace('Send channel state is: ' + readyState);
-  if (readyState === 'open') {
-    sendData();
-  }
-}
-
-function onReceiveChannelStateChange() {
-  var readyState = receiveChannel.readyState;
-  trace('Receive channel state is: ' + readyState);
-  if (readyState === 'open') {
-    timestampStart = (new Date()).getTime();
-    timestampPrev = timestampStart;
-    statsInterval = window.setInterval(displayStats, 500);
-    window.setTimeout(displayStats, 100);
-    window.setTimeout(displayStats, 300);
-  }
-}
-
-// display bitrate statistics.
-function displayStats() {
-  var display = function(bitrate) {
-    bitrateDiv.innerHTML = '<strong>Current Bitrate:</strong> ' +
-        bitrate + ' kbits/sec';
-  };
-
-  if (remoteConnection && remoteConnection.iceConnectionState === 'connected') {
-    if (adapter.browserDetails.browser === 'chrome') {
-      // TODO: once https://code.google.com/p/webrtc/issues/detail?id=4321
-      // lands those stats should be preferrred over the connection stats.
-      remoteConnection.getStats(null, function(stats) {
-        for (var key in stats) {
-          var res = stats[key];
-          if (timestampPrev === res.timestamp) {
-            return;
-          }
-          if (res.type === 'googCandidatePair' &&
-              res.googActiveConnection === 'true') {
-            // calculate current bitrate
-            var bytesNow = res.bytesReceived;
-            var bitrate = Math.round((bytesNow - bytesPrev) * 8 /
-                (res.timestamp - timestampPrev));
-            display(bitrate);
-            timestampPrev = res.timestamp;
-            bytesPrev = bytesNow;
-            if (bitrate > bitrateMax) {
-              bitrateMax = bitrate;
-            }
-          }
-        }
-      });
-    } else {
-      // Firefox currently does not have data channel stats. See
-      // https://bugzilla.mozilla.org/show_bug.cgi?id=1136832
-      // Instead, the bitrate is calculated based on the number of
-      // bytes received.
-      var bytesNow = receivedSize;
-      var now = (new Date()).getTime();
-      var bitrate = Math.round((bytesNow - bytesPrev) * 8 /
-          (now - timestampPrev));
-      display(bitrate);
-      timestampPrev = now;
-      bytesPrev = bytesNow;
-      if (bitrate > bitrateMax) {
-        bitrateMax = bitrate;
-      }
-    }
-  }
 }
